421710b3a1a5b7027cedb6777e8c2fe43cdb41ac
Hyeongseok Son <sonhs@postech.ac.kr>
2021-04-05 12:53:53 +0900

    add training codes

--- main.py
+++ main.py
@@ -41,6 +41,121 @@ def read_all_imgs(img_list, path='', n_threads=32, mode = 'RGB'):
     return imgs
 
 
+def blurmap_3classes(index):
+    print "Blurmap Generation"
+    
+    date = datetime.datetime.now().strftime("%y.%m.%d")
+    save_dir_sample = './output'
+    tl.files.exists_or_mkdir(save_dir_sample)
+
+    #Put the input path!
+    sharp_path = './input'
+    test_sharp_img_list = os.listdir(sharp_path)
+    test_sharp_img_list.sort()
+
+
+    flag=0
+    i=0
+
+
+    for image in test_sharp_img_list:
+        if(i>=index and i<index+100):
+            print i
+            if (image.find('.jpg') & image.find('.png') & image.find('.JPG')&image.find('.PNG')) is not -1:
+
+                sharp = os.path.join(sharp_path, image)
+                sharp_image = Image.open(sharp)
+                sharp_image.load()
+
+                sharp_image = np.asarray(sharp_image, dtype="float32")
+               
+                if(len(sharp_image.shape)<3):
+                    sharp_image= np.expand_dims(np.asarray(sharp_image), 3)
+                    sharp_image=np.concatenate([sharp_image, sharp_image, sharp_image],axis=2)
+
+                if (sharp_image.shape[2] ==4):
+                    print sharp_image.shape
+                    sharp_image = np.expand_dims(np.asarray(sharp_image), 3)
+
+                    print sharp_image.shape
+                    sharp_image = np.concatenate((sharp_image[:,:,0],sharp_image[:,:,1],sharp_image[:,:,2]),axis=2)
+
+                print sharp_image.shape
+
+                image_h, image_w =sharp_image.shape[0:2]
+                print image_h, image_w
+
+                test_image = sharp_image[0: image_h-(image_h%16), 0: 0 + image_w-(image_w%16), :]/(255.)
+
+                # Model
+                patches_blurred = tf.placeholder('float32', [1, test_image.shape[0], test_image.shape[1], 3], name='input_patches')
+                if flag==0:
+                    reuse =False
+                else:
+                    reuse =True
+
+                start_time = time.time()
+
+                with tf.variable_scope('Unified') as scope:
+                    with tf.variable_scope('VGG') as scope3:
+                        n, f0, f0_1, f1_2, f2_3, hrg, wrg = VGG19_pretrained(patches_blurred, reuse=reuse,scope=scope3)
+                        #tl.visualize.draw_weights(n.all_params[0].eval(), second=10, saveable=True, name='weight_of_1st_layer', fig_idx=2012)
+
+                    with tf.variable_scope('UNet') as scope1:
+                        output,m1,m2,m3= Decoder_Network_classification(n, f0, f0_1, f1_2, f2_3 ,hrg,wrg, reuse = reuse, scope = scope1)
+                   
+                    output_map = tf.nn.softmax(output.outputs)
+                    output_map1 = tf.nn.softmax(m1.outputs)
+                    output_map2 = tf.nn.softmax(m2.outputs)
+                    output_map3 = tf.nn.softmax(m3.outputs)
+
+                a_vars = tl.layers.get_variables_with_name('Unified', False, True)
+
+                saver = tf.train.Saver()
+                sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=False))
+                tl.layers.initialize_global_variables(sess)
+
+                # Load checkpoint
+                saver.restore(sess, "./model/final_model.ckpt")
+
+
+
+                start_time = time.time()
+                blur_map,o1,o2,o3 = sess.run([output_map,output_map1,output_map2,output_map3],{patches_blurred: np.expand_dims(
+                    (test_image ), axis=0)})
+                blur_map = np.squeeze(blur_map )
+                o1= np.squeeze(o1)
+                o2 = np.squeeze(o2)
+                o3 = np.squeeze(o3)
+
+        
+
+                if ".jpg" in image:
+                    image.replace(".jpg", ".png")
+                    cv2.imwrite(save_dir_sample +  '/'+ image.replace(".jpg", ".png"), blur_map*255)
+                    
+                if ".JPG" in image:
+                    image.replace(".JPG", ".png")
+                    cv2.imwrite(save_dir_sample +  '/'+ image.replace(".JPG", ".png"), blur_map*255)
+                if ".PNG" in image:
+                    image.replace(".jpg", ".png")
+                    
+                    cv2.imwrite(save_dir_sample +  '/'+ image.replace(".jpg", ".png"), blur_map*255)
+                
+
+                sess.close()
+                flag=1
+
+
+                print("5.--- %s seconds ---" % (time.time() - start_time))
+                start_time = time.time()
+                if(i==index+101-1):
+                    return 0
+
+        i = i + 1
+
+    return 0
+
 def train_with_CUHK():
     checkpoint_dir ="test_checkpoint/{}".format(tl.global_flag['mode'])  # checkpoint_resize_conv
     tl.files.exists_or_mkdir(checkpoint_dir)
@@ -450,121 +565,6 @@ def train_with_synthetic():
         if epoch % 10== 0:
             tl.files.save_ckpt(sess=sess, mode_name='SA_net_{}.ckpt'.format(tl.global_flag['mode']), save_dir = checkpoint_dir, var_list = a_vars, global_step = global_step, printable = False)
 
-
-def blurmap_3classes(index):
-    print "Blurmap Generation"
-    
-    date = datetime.datetime.now().strftime("%y.%m.%d")
-    save_dir_sample = './output'
-    tl.files.exists_or_mkdir(save_dir_sample)
-
-    #Put the input path!
-    sharp_path = './input'
-    test_sharp_img_list = os.listdir(sharp_path)
-    test_sharp_img_list.sort()
-
-
-    flag=0
-    i=0
-
-
-    for image in test_sharp_img_list:
-        if(i>=index and i<index+100):
-            print i
-            if (image.find('.jpg') & image.find('.png') & image.find('.JPG')&image.find('.PNG')) is not -1:
-
-                sharp = os.path.join(sharp_path, image)
-                sharp_image = Image.open(sharp)
-                sharp_image.load()
-
-                sharp_image = np.asarray(sharp_image, dtype="float32")
-               
-                if(len(sharp_image.shape)<3):
-                    sharp_image= np.expand_dims(np.asarray(sharp_image), 3)
-                    sharp_image=np.concatenate([sharp_image, sharp_image, sharp_image],axis=2)
-
-                if (sharp_image.shape[2] ==4):
-                    print sharp_image.shape
-                    sharp_image = np.expand_dims(np.asarray(sharp_image), 3)
-
-                    print sharp_image.shape
-                    sharp_image = np.concatenate((sharp_image[:,:,0],sharp_image[:,:,1],sharp_image[:,:,2]),axis=2)
-
-                print sharp_image.shape
-
-                image_h, image_w =sharp_image.shape[0:2]
-                print image_h, image_w
-
-                test_image = sharp_image[0: image_h-(image_h%16), 0: 0 + image_w-(image_w%16), :]/(255.)
-
-                # Model
-                patches_blurred = tf.placeholder('float32', [1, test_image.shape[0], test_image.shape[1], 3], name='input_patches')
-                if flag==0:
-                    reuse =False
-                else:
-                    reuse =True
-
-                start_time = time.time()
-
-                with tf.variable_scope('Unified') as scope:
-                    with tf.variable_scope('VGG') as scope3:
-                        n, f0, f0_1, f1_2, f2_3, hrg, wrg = VGG19_pretrained(patches_blurred, reuse=reuse,scope=scope3)
-                        #tl.visualize.draw_weights(n.all_params[0].eval(), second=10, saveable=True, name='weight_of_1st_layer', fig_idx=2012)
-
-                    with tf.variable_scope('UNet') as scope1:
-                        output,m1,m2,m3= Decoder_Network_classification(n, f0, f0_1, f1_2, f2_3 ,hrg,wrg, reuse = reuse, scope = scope1)
-                   
-                    output_map = tf.nn.softmax(output.outputs)
-                    output_map1 = tf.nn.softmax(m1.outputs)
-                    output_map2 = tf.nn.softmax(m2.outputs)
-                    output_map3 = tf.nn.softmax(m3.outputs)
-
-                a_vars = tl.layers.get_variables_with_name('Unified', False, True)
-
-                saver = tf.train.Saver()
-                sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=False))
-                tl.layers.initialize_global_variables(sess)
-
-                # Load checkpoint
-                saver.restore(sess, "./model/final_model.ckpt")
-
-
-
-                start_time = time.time()
-                blur_map,o1,o2,o3 = sess.run([output_map,output_map1,output_map2,output_map3],{patches_blurred: np.expand_dims(
-                    (test_image ), axis=0)})
-                blur_map = np.squeeze(blur_map )
-                o1= np.squeeze(o1)
-                o2 = np.squeeze(o2)
-                o3 = np.squeeze(o3)
-
-        
-
-                if ".jpg" in image:
-                    image.replace(".jpg", ".png")
-                    cv2.imwrite(save_dir_sample +  '/'+ image.replace(".jpg", ".png"), blur_map*255)
-                    
-                if ".JPG" in image:
-                    image.replace(".JPG", ".png")
-                    cv2.imwrite(save_dir_sample +  '/'+ image.replace(".JPG", ".png"), blur_map*255)
-                if ".PNG" in image:
-                    image.replace(".jpg", ".png")
-                    
-                    cv2.imwrite(save_dir_sample +  '/'+ image.replace(".jpg", ".png"), blur_map*255)
-                
-
-                sess.close()
-                flag=1
-
-
-                print("5.--- %s seconds ---" % (time.time() - start_time))
-                start_time = time.time()
-                if(i==index+101-1):
-                    return 0
-
-        i = i + 1
-
-    return 0
 
 if __name__ == '__main__':
     import argparse
